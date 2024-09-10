abda25e55e2d8913c03ae142194b7045a1ee75e1
SachinKoli1 <154689037+SachinKoli1@users.noreply.github.com>
2024-09-10 21:30:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(event, tabname) {
+    for (var tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for (var tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var icon=document.getElementById("icon");
+    icon.onclick=function(){
+        document.body.classList.toggle("dark-theme");
+}
